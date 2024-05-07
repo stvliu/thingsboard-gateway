f7b19e9b75ebbbe06e995f2c2446153876ea391d
liuye <stvliu@21cn.com>
2024-05-07 22:34:30 +0800

    Update hdeac_connector.py

--- thingsboard_gateway/extensions/hdeac/hdeac_connector.py
+++ thingsboard_gateway/extensions/hdeac/hdeac_connector.py
@@ -1,5 +1,5 @@
 """
-HDE-AIR系列机柜空调连接器，用于通过ThingsBoard IoT Gateway采集和控制黑盾机柜空调。
+HDE-AIR系列机柜空调连接器,用于通过ThingsBoard IoT Gateway采集和控制黑盾机柜空调。
 """
 
 import time
@@ -14,7 +14,7 @@ from thingsboard_gateway.tb_utility.tb_logger import init_logger
 
 class HdeAcConnector(Connector):
     """
-    HDE-AIR系列机柜空调连接器类，继承自Connector基类，实现了黑盾机柜空调的数据采集和控制逻辑。
+    HDE-AIR系列机柜空调连接器类,继承自Connector基类,实现了黑盾机柜空调的数据采集和控制逻辑。
     """
 
     def __init__(self, gateway, config, connector_type):
@@ -23,14 +23,14 @@ class HdeAcConnector(Connector):
         
         参数:
         - gateway: ThingsBoard IoT Gateway实例
-        - config: 连接器配置信息，从tb_gateway.yaml中读取
-        - connector_type: 连接器类型，用于在日志中区分不同的连接器
+        - config: 连接器配置信息,从tb_gateway.yaml中读取
+        - connector_type: 连接器类型,用于在日志中区分不同的连接器
         """
         super().__init__()  
         self.gateway = gateway
         self.config = config
         self.connector_type = connector_type
-        # 使用从配置中读取的连接器名称，如果没有指定，则默认为HdeAcConnector
+        # 使用从配置中读取的连接器名称,如果没有指定,则默认为HdeAcConnector
         self.name = config.get("name", "HdeAcConnector") 
         self.devices = config.get('devices')
         
@@ -53,11 +53,16 @@ class HdeAcConnector(Connector):
         self.__stopped = False  
         self.__serial = None
         
+        # 增加连接重试机制相关的属性
+        self.__connecting = False
+        self.__last_connect_attempt_time = 0
+        self.__reconnect_interval = config.get('reconnect_interval', 10)
+        
     def open(self):
         """
         启动连接器。
         
-        将连接器设置为启动状态，并启动连接器线程。
+        将连接器设置为启动状态,并启动连接器线程。
         """
         self.__stopped = False
         self.start()
@@ -66,7 +71,7 @@ class HdeAcConnector(Connector):
         """  
         关闭连接器。
         
-        将连接器设置为停止状态，关闭RS485串口连接。
+        将连接器设置为停止状态,关闭RS485串口连接。
         """
         self.__stopped = True
         self.__disconnect()
@@ -76,7 +81,7 @@ class HdeAcConnector(Connector):
         获取连接器名称。
         
         返回:
-        - 连接器名称，字符串  
+        - 连接器名称,字符串  
         """
         return self.name
     
@@ -85,7 +90,7 @@ class HdeAcConnector(Connector):
         获取连接器ID。
         
         返回:  
-        - 连接器ID，字符串
+        - 连接器ID,字符串
         """  
         return self.config.get('id', self.name)
     
@@ -94,7 +99,7 @@ class HdeAcConnector(Connector):
         获取连接器类型。
         
         返回:  
-        - 连接器类型，字符串
+        - 连接器类型,字符串
         """
         return self.connector_type
     
@@ -103,7 +108,7 @@ class HdeAcConnector(Connector):
         获取连接器配置信息。
         
         返回:
-        - 连接器配置信息，字典
+        - 连接器配置信息,字典
         """
         return self.config
     
@@ -112,7 +117,7 @@ class HdeAcConnector(Connector):
         获取连接器连接状态。
         
         返回:
-        - 连接器是否已连接，布尔值
+        - 连接器是否已连接,布尔值
         """
         return self.__connected
     
@@ -121,7 +126,7 @@ class HdeAcConnector(Connector):
         获取连接器停止状态。
         
         返回:
-        - 连接器是否已停止，布尔值  
+        - 连接器是否已停止,布尔值  
         """
         return self.__stopped
             
@@ -130,7 +135,7 @@ class HdeAcConnector(Connector):
         处理属性更新请求。
         
         参数:
-        - content: 属性更新请求的内容，字典
+        - content: 属性更新请求的内容,字典
         """
         try:
             device = tuple(filter(lambda d: d['name'] == content['device'], self.devices))[0]
@@ -146,7 +151,7 @@ class HdeAcConnector(Connector):
         处理RPC请求。
         
         参数:  
-        - content: RPC请求的内容，字典
+        - content: RPC请求的内容,字典
         """
         try:
             device = tuple(filter(lambda d: d['name'] == content['device'], self.devices))[0]
@@ -164,8 +169,8 @@ class HdeAcConnector(Connector):
         发送统计数据。
         
         参数:
-        - connector_name: 连接器名称，字符串
-        - data: 要发送的数据，字典  
+        - connector_name: 连接器名称,字符串
+        - data: 要发送的数据,字典  
         """
         self.gateway.statistics['MessagesReceived'] += 1
         self.gateway.send_to_storage(connector_name, data) 
@@ -173,16 +178,19 @@ class HdeAcConnector(Connector):
     
     def __run(self):
         """
-        连接器线程主函数，循环执行数据采集和发送。
+        连接器线程主函数,循环执行数据采集和发送。
         """
         while not self.__stopped:
-            # 如果连接器处于未连接状态，尝试建立连接
-            if not self.__connected:  
-                self.__connect()
+            if not self.__connected:
+                if not self.__connecting and time.time() - self.__last_connect_attempt_time >= self.__reconnect_interval:
+                    self.__connecting = True
+                    self.__connect()
+                else:
+                    time.sleep(1)
+                    continue
             else:
-                # 如果连接器已连接，执行一次数据采集和发送  
                 self.__get_data()
-            # 等待指定的采集间隔时间    
+                
             time.sleep(self.__interval)
             
     def __connect(self):
@@ -190,16 +198,17 @@ class HdeAcConnector(Connector):
         与机柜空调建立RS485连接。
         """
         try:
-            # 使用serial.Serial对象，根据配置参数打开RS485串口
             self.__serial = serial.Serial(port=self.__port, baudrate=self.__baudrate, timeout=self.__timeout, 
                                           method=self.__method, stopbits=self.__stopbits, bytesize=self.__bytesize, 
                                           parity=self.__parity)
-            # 标记已连接状态为True  
             self.__connected = True
+            self.__connecting = False
             self.log.info('Connected to serial port %s', self.__port)
         except Exception as e:
             self.log.error('Failed to connect to serial port %s: %s', self.__port, str(e))
-            time.sleep(10)
+            self.__connected = False
+            self.__connecting = False
+            self.__last_connect_attempt_time = time.time()
             
     def __disconnect(self):
         """
@@ -238,10 +247,10 @@ class HdeAcConnector(Connector):
         向机柜空调发送控制命令并接收响应。
         
         参数:
-        - command: 要发送的控制命令，字节数组
+        - command: 要发送的控制命令,字节数组
         
         返回:  
-        - 机柜空调返回的数据，字节数组
+        - 机柜空调返回的数据,字节数组
         """
         self.log.debug('Writing command to serial port: %s', command.hex())
         self.__serial.write(command)  
